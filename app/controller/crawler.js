263c4470e5391bbb60ff610eb27f9c5f9c384d75
unknown <1169470657@qq.com>
2018-12-01 12:58:39 +0800

    add crawlerMsg

--- app/controller/crawler.js
+++ app/controller/crawler.js
@@ -16,6 +16,160 @@ class CrawlerController extends Controller {
     return cookie;
   }
 
+  //爬取成绩的函数
+  async crawlerScore() {
+    const ctx = this.ctx;
+    const Course = ctx.model.Course;
+    // let USERNAME;
+    // let PASSWORD;
+    //临时赋值
+    let USERNAME = '2017211503';
+    let PASSWORD = '799..com';
+    const data = {
+      "USERNAME": USERNAME,
+      "PASSWORD": PASSWORD
+    }
+    //获取cookie
+    const cookie = await this.getCookie();
+    console.log(cookie);
+    //模拟登录
+    //发送账号和密码
+    const login = await axios({
+      url: 'http://jwcnew.nefu.edu.cn/dblydx_jsxsd/xk/LoginToXk',
+      method: 'POST',
+      data: qs.stringify(data),
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Cookie': cookie,
+        'Host': 'jwcnew.nefu.edu.cn',
+        'Origin': 'http://jwcnew.nefu.edu.cn',
+        'Referer': 'http://jwcnew.nefu.edu.cn/dblydx_jsxsd/',
+      },
+    })
+
+    const getScore = await axios({
+      url: 'http://jwcnew.nefu.edu.cn/dblydx_jsxsd/kscj/cjcx_list',
+      method: 'get',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Cookie': cookie,
+        'Host': 'jwcnew.nefu.edu.cn',
+        'Origin': 'http://jwcnew.nefu.edu.cn',
+        'Referer': 'http://jwcnew.nefu.edu.cn/dblydx_jsxsd/kscj/cjcx_query?Ves632DSdyV=NEW_XSD_XJCJ',
+      },
+      data: 'kcxz=&kcmc=&xsfs=max&isfx=0'
+    });
+    const html = getScore.data;
+    const $ = cheerio.load(html, { decodeEntities: false });
+    //获取专业课与成绩
+    $("#dataList").find("tr").each((index, element) => {
+      $(element).find("td").each((index2, element2) => {
+        if(index2 === 3 || index2 === 4) {
+          console.log($(element2).text());
+        }
+      })
+    })
+  }
+  //爬取个人信息的函数
+  async crawlerMessage() {
+    const ctx = this.ctx;
+    const Course = ctx.model.Course;
+    // let USERNAME;
+    // let PASSWORD;
+    //临时赋值
+    let USERNAME = '2017211503';
+    let PASSWORD = '799..com';
+    const data = {
+      "USERNAME": USERNAME,
+      "PASSWORD": PASSWORD
+    }
+    //获取cookie
+    const cookie = await this.getCookie();
+    console.log(cookie);
+    //模拟登录
+    //发送账号和密码
+    const login = await axios({
+      url: 'http://jwcnew.nefu.edu.cn/dblydx_jsxsd/xk/LoginToXk',
+      method: 'POST',
+      data: qs.stringify(data),
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Cookie': cookie,
+        'Host': 'jwcnew.nefu.edu.cn',
+        'Origin': 'http://jwcnew.nefu.edu.cn',
+        'Referer': 'http://jwcnew.nefu.edu.cn/dblydx_jsxsd/',
+      },
+    })
+
+    const getMsg = await axios({
+      url: 'http://jwcnew.nefu.edu.cn/dblydx_jsxsd/grxx/xsxx',
+      method: 'get',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Cookie': cookie,
+        'Host': 'jwcnew.nefu.edu.cn',
+        'Origin': 'http://jwcnew.nefu.edu.cn',
+        'Referer': 'http://jwcnew.nefu.edu.cn/dblydx_jsxsd/kscj/cjcx_query?Ves632DSdyV=NEW_XSD_XJCJ',
+      },
+      data: 'kcxz=&kcmc=&xsfs=max&isfx=0'
+    });
+    const html = getMsg.data;
+    const $ = cheerio.load(html, { decodeEntities: false });
+    const msg = {
+      '院系': '',
+      '专业': '',
+      '学制': '',
+      '学号': '',
+      '姓名': '',
+      '性别': '',
+      '姓名拼音': '',
+      '出生日期': '',
+      '本人电话': '',
+      '政治面貌': '',
+      '籍贯': '',
+      '民族': '',
+      '家庭电话': '',
+      '入学日期': '',
+      '入学考号': '',
+      '身份证编号': '',
+    }
+    $("tbody").find("td").each((index, element) => {
+      if(index === 10) {
+        msg["院系"] = $(element).text().split("：")[1];
+      }else if(index === 11){
+        msg["专业"] = $(element).text().split("：")[1];
+      }else if(index === 12){
+        msg["学制"] = $(element).text().split("：")[1];
+      }else if(index === 14){
+        msg["学号"] = $(element).text().split("：")[1];
+      }else if(index === 16){
+        msg["姓名"] = $(element).text();
+      }else if(index === 18){
+        msg["性别"] = $(element).text();
+      }else if(index === 20){
+        msg["姓名拼音"] = $(element).text();
+      }else if(index === 23){
+        msg["出生日期"] = $(element).text();
+      }else if(index === 27){
+        msg["本人电话"] = $(element).text();
+      }else if(index === 31){
+        msg["政治面貌"] = $(element).text();
+      }else if(index === 33){
+        msg["籍贯"] = $(element).text();
+      }else if(index === 37){
+        msg["民族"] = $(element).text();
+      }else if(index === 51){
+        msg["家庭电话"] = $(element).text();
+      }else if(index === 190){
+        msg["入学日期"] = $(element).text();
+      }else if(index === 194){
+        msg["入学考号"] = $(element).text();
+      }else if(index === 196){
+        msg["身份证编号"] = $(element).text();
+      }
+    });
+    // console.log(msg);
+  }
   async login() {
     const ctx = this.ctx;
     const Course = ctx.model.Course;
@@ -71,7 +225,6 @@ class CrawlerController extends Controller {
     });
     async function getLessonArr(getScore) {
       const html = getLesson.data;
-      // const html = getScore.data;
 
       //load()中的第二个参数防止中文转码
       const $ = cheerio.load(html, { decodeEntities: false });
@@ -80,30 +233,19 @@ class CrawlerController extends Controller {
       const stuId = $('#Top1_divLoginName').text().substring(3, 13);
       // 获取学期id
       const semesterId = $('#xnxq01id').val();
-      // console.log(semesterId);
-      // console.log(stuId);
-      // for (let index1 = 0; index1 < $(".kbcontent").length; index1++) {
-
-
-      // }
-      // console.log($(".kbcontent"));
       let courseArr = [];
       $(".kbcontent").map(async (index1, element1) => {
-        // console.log(index1);
 
         const lessonName = $(element1).contents().filter(function () {
           return this.nodeType == 3;
         }).text();
-        // console.log(lessonName);
         const lessonNameArr = lessonName.split("---------------------");
         for (let index2 = 0; index2 < lessonNameArr.length; index2++) {
 
           const element2 = lessonNameArr[index2];
 
-          // console.log(element2);
           if (element2.length <= 2) {
             // 没课
-            // console.log('没课');
           } else {
             let course = {
               schoolId: 'nefu',
@@ -112,30 +254,18 @@ class CrawlerController extends Controller {
               teacher: '',
               name: '',
               injectByUser: false,
-              // courseUnits: [{
-              //   weeks: String,
-              //   dayOfWeek: String,
-              //   timeStart: String, // 节次
-              //   count: String, // 节数
-              //   building: String,
-              //   room: String,
-              //   smartWeeks: Array,
-              // }],
               courseUnits: []
             };
 
             // 存stuId
-            // console.log($('#Top1_divLoginName').text());
             course['stuId'] = stuId;
             // 存学期id
             course['semesterId'] = semesterId;
             // 存课程名
             course['name'] = element2;
-            // console.log(element2);
             // 存老师和教室
             $(element1).find('font[title="老师"]').each((index3, element3) => {
               if (index3 === index2) {
-                // console.log($(element3).text());
                 course['teacher'] = $(element3).text();
               }
             });
@@ -150,21 +280,15 @@ class CrawlerController extends Controller {
             }
             $(element1).find('font[title="周次(节次)"]').each(async (index3, element3) => {
               if (index3 === index2) {
-                // console.log($(element3).text());
                 const weeks = $(element3).text();
-                // console.log(weeks);
                 courseUnitsItem['weeks'] = weeks;
 
                 //处理智能周
                 let smartWeeks = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
                 const weekStr = weeks.slice(0, -3);
-                // console.log(weekStr);
-                // console.log(weekStr.indexOf(','));
                 if (weekStr.indexOf(',') === -1) {
                   //周数中没有','的字符串
-                  // console.log(weekStr);
                   const weekArr = weekStr.split('-');
-                  // console.log(weekArr);
                   if (weekArr.length === 1) {
                     //只有一个数字的周数
                     const smartWeeksIndex = Number(weekArr[0]) - 1;
@@ -179,7 +303,6 @@ class CrawlerController extends Controller {
                   }
                 } else {
                   // 周数中有','的字符串
-                  // console.log(weekStr.split(','));
                   const weekArr = weekStr.split(',');
                   weekArr.map((e, i) => {
                     const smartWeeksIndex = Number(e) - 1;
@@ -187,18 +310,15 @@ class CrawlerController extends Controller {
                   })
                 }
                 courseUnitsItem['smartWeeks'] = smartWeeks;
-                // console.log(smartWeeks);
               }
             });
             $(element1).find('font[title="教室"]').each((index3, element3) => {
               if (index3 === index2) {
-                // console.log($(element3).text());
                 const place = $(element3).text();
                 const building = place.slice(0, -3);
                 const room = place.slice(-3);
                 courseUnitsItem['building'] = building;
                 courseUnitsItem['room'] = room;
-                // console.log(building, room);
               }
             });
             let timeStart;
@@ -222,9 +342,7 @@ class CrawlerController extends Controller {
               // console.log('第十一十二节');
               timeStart = '11';
             };
-            // console.log(timeStart);
             courseUnitsItem['timeStart'] = timeStart;
-            // console.log(courseUnitsItem);
             let dayOfWeek;
             switch (index1) {
               case 0:
@@ -233,7 +351,6 @@ class CrawlerController extends Controller {
               case 21:
               case 28:
               case 35:
-                // console.log('星期一');
                 dayOfWeek = '星期一';
                 break;
               case 1:
@@ -242,7 +359,6 @@ class CrawlerController extends Controller {
               case 22:
               case 29:
               case 36:
-                // console.log('星期二');
                 dayOfWeek = '星期二';
                 break;
               case 2:
@@ -251,7 +367,6 @@ class CrawlerController extends Controller {
               case 23:
               case 30:
               case 37:
-                // console.log('星期三');
                 dayOfWeek = '星期三';
                 break;
               case 3:
@@ -260,7 +375,6 @@ class CrawlerController extends Controller {
               case 24:
               case 31:
               case 38:
-                // console.log('星期四');
                 dayOfWeek = '星期四';
                 break;
               case 4:
@@ -269,7 +383,6 @@ class CrawlerController extends Controller {
               case 25:
               case 32:
               case 39:
-                // console.log('星期五');
                 dayOfWeek = '星期五';
                 break;
               case 5:
@@ -278,7 +391,6 @@ class CrawlerController extends Controller {
               case 26:
               case 33:
               case 40:
-                // console.log('星期六');
                 dayOfWeek = '星期六';
                 break;
               case 6:
@@ -287,7 +399,6 @@ class CrawlerController extends Controller {
               case 27:
               case 34:
               case 41:
-                // console.log('星期日');
                 dayOfWeek = '星期日';
                 break;
             }
@@ -305,7 +416,7 @@ class CrawlerController extends Controller {
         html: $.html()
       });
     }
-    
+
     let html = await getLessonArr(getScore);
     html = html.html;
 
@@ -320,7 +431,7 @@ class CrawlerController extends Controller {
         let courseArrIndex;
         for (let j = 0; j < newCourseArr.length; j++) {
           const e2 = newCourseArr[j];
-          if(e1.schoolId === e2.schoolId && e1.stuId === e2.stuId && e1.semesterId === e2.semesterId && e1.teacher === e2.teacher && e1.name === e2.name) {
+          if (e1.schoolId === e2.schoolId && e1.stuId === e2.stuId && e1.semesterId === e2.semesterId && e1.teacher === e2.teacher && e1.name === e2.name) {
             flag = false;
             courseArrIndex = j;
             let oldCourseUnits1 = e1.courseUnits;
@@ -328,16 +439,15 @@ class CrawlerController extends Controller {
             e1.courseUnits = [...oldCourseUnits1, ...oldCourseUnits2];
           };
         }
-        if(flag) {
+        if (flag) {
           newCourseArr.push(e1);
-        }else {
+        } else {
           newCourseArr[courseArrIndex] = e1;
         }
       }
-      // console.log(newCourseArr);
       return (newCourseArr);
     }
-    
+
     const newCourseArr = await formatCourseArr();
 
     async function saveCourse() {
@@ -358,66 +468,8 @@ class CrawlerController extends Controller {
     }
 
     // await saveCourse();
-
-    // console.log(courseArr.length);
-    // const courseSchema = new Course({
-    //   schoolId: course.schoolId,
-    //   stuId: course.stuId,
-    //   semesterId: course.semesterId,
-    //   teacher: course.teacher,
-    //   name: course.name,
-    //   injectByUser: course.injectByUser,
-    //   courseUnits: course.courseUnits
-    // });
-    // await courseSchema.save();
-    // const arr = await Course.find();
-    // const courseArr = await Course.find({
-    //   schoolId: course.schoolId,
-    //   stuId: course.stuId,
-    //   semesterId: course.semesterId,
-    //   teacher: course.teacher,
-    //   name: course.name,
-    // });
-    // console.log(courseArr.length);
-    // if (courseArr.length === 0) {
-    //   // 没有课程， 直接储存
-    //   // console.log('新课');
-    //   await Course.create({
-    //     schoolId: course.schoolId,
-    //     stuId: course.stuId,
-    //     semesterId: course.semesterId,
-    //     teacher: course.teacher,
-    //     name: course.name,
-    //     injectByUser: course.injectByUser,
-    //     courseUnits: course.courseUnits
-    //   });
-    // } else {
-    //   // 有课程，先获取oldCourseUnits，再把新的courseUnits push 到里面，最后update
-    //   // console.log('老课');
-    //   let newCourseUnits = course.courseUnits;
-    //   //更新Course在courseUnits里添加一项
-    //   await Course.updateOne({
-    //     schoolId: course.schoolId,
-    //     stuId: course.stuId,
-    //     semesterId: course.semesterId,
-    //     teacher: course.teacher,
-    //     name: course.name,
-    //   }, {
-    //       $push: {
-    //         courseUnits: newCourseUnits
-    //       }
-    //     })
-    // }
-
-    //获取专业课与成绩
-    // $("#dataList").find("tr").each((index, element) => {
-    //   $(element).find("td").each((index2, element2) => {
-    //     if(index2 === 3 || index2 === 4) {
-    //       console.log($(element2).text());
-    //     }
-    //   })
-    // })
-    // console.log(html);
+    // await this.crawlerScore();
+    // await this.crawlerMessage();
     const reqData = {
       message: "ok",
       newCourseArr: newCourseArr,
